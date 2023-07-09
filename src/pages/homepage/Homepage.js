d0bf61fcc526431c18bb13a3fbc787868ffbbae3
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-07-09 16:33:23 -0400

    created homepage

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,12 @@
+import Header from "../../components/header/Header";
+import "../homepage/Homepage.scss";
+
+function HomePage() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default HomePage;
