917076afeda97d0055a2480a5d62106502baa86c
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-07-18 23:43:52 -0400

    finished styling the hero component

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -4,24 +4,20 @@ import "./Hero.scss";
 export default function Hero() {
   return (
     <div className="hero">
-      <img
-        className="hero__thumbnail"
-        src="https://media.giphy.com/media/hugQmENCWTyPM3yG5D/giphy-downsized-large.gif"
-      />
       <div className="hero__title-container">
         <h1 className="hero__title">Tech Entrepreneur.</h1>
         <h1 className="hero__title">Software Developer.</h1>
         <h1 className="hero__title">From Toronto, Ontario.</h1>
       </div>
-      <div>
+      <div className="hero__align-container">
         <ul className="hero__links-container">
-          <a href="/" className="header__links">
+          <a href="/" className="hero__links">
             About Me
           </a>
-          <a href="/" className="header__links">
+          <a href="/" className="hero__links">
             Projects
           </a>
-          <a href="/" className="header__links">
+          <a href="/" className="hero__links">
             Contact
           </a>
         </ul>
