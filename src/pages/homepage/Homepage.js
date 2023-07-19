81d6775b4f1cea7a468699243c0a0bb16697db52
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-07-18 23:47:27 -0400

    added logic for splash screen & created homepage

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,10 +1,34 @@
-import Header from "../../components/header/Header";
+import { useEffect, useState } from "react";
+import Footer from "../../components/footer/Footer";
+import Hero from "../../components/hero/Hero";
+import Splash from "../../components/splash/SplashScreen";
+import Particles from "../../components/particles/Particles";
 import "../homepage/Homepage.scss";
+import Projects from "../../components/projects/Projects";
+import About from "../../components/about/About";
 
 function HomePage() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+  }, []);
+
   return (
     <>
-      <Header />
+      {isLoading ? (
+        <Splash />
+      ) : (
+        <div>
+          <Particles id="tsparticles" />
+          <Hero />
+          <About />
+          <Projects />
+          <Footer />
+        </div>
+      )}
     </>
   );
 }
