db22c780d0a6ea0f2116b41edd746a9b52254b5e
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-07-29 19:43:02 -0400

    created splash screen animation

--- src/components/splash/SplashScreen.js
+++ src/components/splash/SplashScreen.js
@@ -1,55 +1,23 @@
 import React from "react";
-import { motion } from "framer-motion";
+import { easeInOut, motion } from "framer-motion";
 import "./SplashScreen.scss";
 
-export default function Splash() {
-  const draw = {
-    hidden: { pathLength: 0, opacity: 0 },
-    visible: (i) => {
-      const delay = 1 + i * 0.5;
-      return {
-        pathLength: 1,
-        opacity: 1,
-        transition: {
-          pathLength: { delay, type: "spring", duration: 1.5, bounce: 0 },
-          opacity: { delay, duration: 0.01 },
-        },
-      };
-    },
-  };
+export default function Splash({ isLoading }) {
   return (
     <div className="splash-screen">
-      <motion.h1
-        className="splash-screen__title"
-        animate={{ y: [50, 10], opacity: 1, scale: 1 }}
-        transition={{
-          type: "tween",
-          ease: "easeOut",
-          duration: 2,
-        }}
-        initial={{ opacity: 0, scale: 0.5 }}
-        whileHover={{ scale: 1.2 }}
+      <motion.div
+        className={`splash-screen__title ${
+          isLoading ? "" : "splash-screen__loaded"
+        }`}
       >
-        Adrien Belcastro
-      </motion.h1>
-      <motion.svg
-        width="1650"
-        height="600"
-        viewBox="0 0 600 600"
-        initial="hidden"
-        animate="visible"
-      >
-        <motion.rect
-          width="260"
-          height="50"
-          x="100"
-          y="200"
-          rx="5"
-          stroke="#ffffff"
-          variants={draw}
-          custom={3}
-        />
-      </motion.svg>
+        <motion.h1
+          initial={{ scale: 0 }}
+          animate={{ scale: isLoading ? 1 : 0 }}
+          transition={{ duration: 1.5 }}
+        >
+          Adrien Belcastro
+        </motion.h1>
+      </motion.div>
     </div>
   );
 }
