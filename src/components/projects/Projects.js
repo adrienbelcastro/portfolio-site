7fd39b592b17d023e39390acd9847cef055ad98e
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-08-01 10:55:52 -0400

    made project section responsive

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,45 +1,44 @@
 import video from "../../assets/videos/project-vid.mp4";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "../projects/Projects.scss";
 
 export default function Projects() {
-  const [isHovered, setIsHovered] = useState(false);
+  const [isDesktop, setIsDesktop] = useState(false);
 
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth >= 768);
+    };
+    handleResize();
 
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <div className="projects">
-      <div
-        className="projects__video-container"
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-      >
+      <div className="projects__video-container">
         <video className="projects__video" autoPlay="autoplay" muted loop>
           <source src={video} type="video/mp4" />
         </video>
-        {isHovered && (
-          <div className="projects__overlay">
-            <h2 className="projects__title">Momentum</h2>
+        <div className={`projects__overlay ${isDesktop ? "" : "active"}`}>
+          <h2 className="projects__title">Momentum</h2>
 
-            <p className="projects__description">
-              Momentum is a recipe app designed to empower users to track their
-              macro-nutrients while enjoying a diverse range of delicious meals.
-              Whether you're striving for a healthier lifestyle or have specific
-              dietary goals, this app provides a comprehensive platform to
-              manage your nutritional intake. The site also features a unique
-              article section into the app. Here, users can contribute their own
-              recipes, share their culinary experiences, and engage in
-              meaningful discussions with like-minded individuals.
-            </p>
-          </div>
-        )}
+          <p className="projects__description">
+            Momentum is a recipe app designed to empower users to track their
+            macro-nutrients while enjoying a diverse range of delicious meals.
+            Whether you're striving for a healthier lifestyle or have specific
+            dietary goals, this app provides a comprehensive platform to manage
+            your nutritional intake. The site also features a unique article
+            section into the app. Here, users can contribute their own recipes,
+            share their culinary experiences, and engage in meaningful
+            discussions with like-minded individuals.
+          </p>
+        </div>
       </div>
     </div>
   );
