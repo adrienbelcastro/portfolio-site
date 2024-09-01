b8b1875163d67d73ce9018e735be067171306aaa
Adrien Belcastro <65431886+adrienbelcastro@users.noreply.github.com>
2024-08-31 20:32:28 -0400

    added alt tag to all tech stack badges

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -24,21 +24,66 @@ export default function About() {
             </p>
             <div className="about__stack">
               <h2 className="about__title"> My Current Tech Stacks</h2>
-              <img src="https://img.shields.io/badge/-HTML5-E34F26?style=flat-square&logo=html5&logoColor=white" />
-              <img src="https://img.shields.io/badge/-CSS3-1572B6?style=flat-square&logo=css3" />
-              <img src="https://img.shields.io/badge/SASS-hotpink.svg?style=flat-square&logo=SASS&logoColor=white" />
-              <img src="https://img.shields.io/badge/vercel-%23000000.svg?style=flat-square&logo=vercel&logoColor=white" />
-              <img src="https://img.shields.io/badge/-JavaScript-black?style=flat-square&logo=javascript" />
-              <img src="https://img.shields.io/badge/-Nodejs-black?style=flat-square&logo=Node.js" />
-              <img src="https://img.shields.io/badge/-React-black?style=flat-square&logo=react" />
-              <img src="https://img.shields.io/badge/-MySQL-black?style=flat-square&logo=mysql" />
-              <img src="https://img.shields.io/badge/-Git-black?style=flat-square&logo=git" />
-              <img src="https://img.shields.io/badge/-GitHub-black?style=flat-square&logo=github" />
-              <img src="https://img.shields.io/badge/-Nextjs-black?style=flat-square&logo=Next.js" />
-              <img src="https://img.shields.io/badge/-TypeScript-black?style=flat-square&logo=typescript" />
-              <img src="https://img.shields.io/badge/-tailwindcss-black?style=flat-square&logo=tailwindcss" />
-              <img src="https://img.shields.io/badge/-PostgreSQL-black?style=flat-square&logo=postgresql" />
-              <img src="https://img.shields.io/badge/-Supabase-black?style=flat-square&logo=supabase" />
+              <img
+                src="https://img.shields.io/badge/-HTML5-E34F26?style=flat-square&logo=html5&logoColor=white"
+                alt="Html badge"
+              />
+              <img
+                src="https://img.shields.io/badge/-CSS3-1572B6?style=flat-square&logo=css3"
+                alt="CSS badge"
+              />
+              <img
+                src="https://img.shields.io/badge/SASS-hotpink.svg?style=flat-square&logo=SASS&logoColor=white"
+                alt="SASS badge"
+              />
+              <img
+                src="https://img.shields.io/badge/vercel-%23000000.svg?style=flat-square&logo=vercel&logoColor=white"
+                alt="Vercel badge"
+              />
+              <img
+                src="https://img.shields.io/badge/-JavaScript-black?style=flat-square&logo=javascript"
+                alt="JS badge"
+              />
+              <img
+                src="https://img.shields.io/badge/-Nodejs-black?style=flat-square&logo=Node.js"
+                alt="Node.js badge"
+              />
+              <img
+                src="https://img.shields.io/badge/-React-black?style=flat-square&logo=react"
+                alt="React badge"
+              />
+              <img
+                src="https://img.shields.io/badge/-MySQL-black?style=flat-square&logo=mysql"
+                alt="MYSQL badge"
+              />
+              <img
+                src="https://img.shields.io/badge/-Git-black?style=flat-square&logo=git"
+                alt="Git badge"
+              />
+              <img
+                src="https://img.shields.io/badge/-GitHub-black?style=flat-square&logo=github"
+                alt="Github badge"
+              />
+              <img
+                src="https://img.shields.io/badge/-Nextjs-black?style=flat-square&logo=Next.js"
+                alt="NextJs badge"
+              />
+              <img
+                src="https://img.shields.io/badge/-TypeScript-black?style=flat-square&logo=typescript"
+                alt="TS badge"
+              />
+              <img
+                src="https://img.shields.io/badge/-tailwindcss-black?style=flat-square&logo=tailwindcss"
+                alt="Tailwind badge"
+              />
+              <img
+                src="https://img.shields.io/badge/-PostgreSQL-black?style=flat-square&logo=postgresql"
+                alt="PostgreSql badge"
+              />
+              <img
+                src="https://img.shields.io/badge/-Supabase-black?style=flat-square&logo=supabase"
+                alt="Supabase badge"
+              />
             </div>
           </div>
         </div>
