0e1b65b05b26dce45da6576d8618dfe13e2a1746
Adrien Belcastro <65431886+adrienbelcastro@users.noreply.github.com>
2025-09-14 11:12:59 -0400

    created dto to transfer project response data

---
+++ backend/src/main/java/ca/adrienbelcastro/domain/dto/ProjectDto.java
@@ -0,0 +1,18 @@
+package ca.adrienbelcastro.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProjectDto {
+    private Long id;
+    private String name;
+    private String description;
+    private String[] techstack;
+    private String url;
+}
