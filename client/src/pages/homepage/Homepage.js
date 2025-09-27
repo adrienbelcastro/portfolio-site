70bd242d9fa8eb370b03f5504a600b0c5bd1b094
Adrien Belcastro <65431886+adrienbelcastro@users.noreply.github.com>
2025-09-27 11:12:59 -0400

    modified homepage to display the particles backgorund along with the new components as per the redesign. Alos modified the splash screen time to be more user frinedly

--- client/src/pages/homepage/Homepage.js
+++ client/src/pages/homepage/Homepage.js
@@ -4,32 +4,39 @@ import Splash from "../../components/splash/SplashScreen";
 import Particles from "../../components/particles/Particles";
 import "./Homepage.scss";
 import Projects from "../../components/projects/Projects";
+
 import About from "../../components/about/About";
 import Contact from "../../components/contact/Contact";
-import CoffeeShop from "../../components/projects/CoffeeShop";
+import Experience from "../../components/experience/Experience";
 
 function HomePage() {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => {
+    const loadingTimer = window.setTimeout(() => {
       setIsLoading(false);
-    }, 4000);
+    }, 3800);
+
+    return () => window.clearTimeout(loadingTimer);
   }, []);
 
   return (
     <>
       {isLoading ? (
-        <Splash isLoading={isLoading} />
+        <div className="homepage">
+          <Particles id="tsparticles" />
+          <Splash isLoading={isLoading} />
+        </div>
       ) : (
-        <div>
+        <main className="homepage__content">
           <Particles id="tsparticles" />
           <Hero />
           <About />
+          <Experience />
           <Projects />
-          <CoffeeShop />
+
           <Contact />
-        </div>
+        </main>
       )}
     </>
   );
