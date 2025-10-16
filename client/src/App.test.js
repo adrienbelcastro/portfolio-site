67a828949f240c21b0c356b9ac3ebfae197821df
Adrien Belcastro <65431886+adrienbelcastro@users.noreply.github.com>
2025-10-16 11:12:59 -0400

    wrote a simple test to learn about testing in javascript

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock("./pages/homepage/Homepage", () => () => <main className="homepage" />);
+
+test("renders the application shell", () => {
+  const { container } = render(<App />);
+
+  expect(container.querySelector(".homepage")).toBeInTheDocument();
 });
