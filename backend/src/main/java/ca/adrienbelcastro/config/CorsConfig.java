fa2cc7a6709259c83dcf771a3f30949836a7d5bc
Adrien Belcastro <65431886+adrienbelcastro@users.noreply.github.com>
2026-07-28 11:35:45 -0400

    modified coors policy to include updated deployed api route

--- backend/src/main/java/ca/adrienbelcastro/config/CorsConfig.java
+++ backend/src/main/java/ca/adrienbelcastro/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package ca.adrienbelcastro.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,20 +12,23 @@ public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                                "https://www.adrienbelcastro.ca"
-                        )
-                        .allowedMethods(
-                                "GET",
-                                "POST",
-                                "PUT",
-                                "DELETE",
-                                "OPTIONS"
-                        )
-                        .allowedHeaders("*");
+                    .allowedOrigins(
+                        "https://www.adrienbelcastro.ca",
+                        "https://adrienbelcastro.ca",
+                        "https://portfolio-site-5gf4ptbug-adrienbelcastros-projects.vercel.app"
+                    )
+                    .allowedMethods(
+                        "GET",
+                        "POST",
+                        "PUT",
+                        "DELETE",
+                        "OPTIONS"
+                    )
+                    .allowedHeaders("*");
             }
         };
     }
